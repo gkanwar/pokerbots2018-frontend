38ae6a613816420d6d849a37c7dddf7e0b2af08a
Tej Kanwar <gurtej@mit.edu>
2018-06-08 23:51:32 -0400

    Adds leaderboard hooks for table outputs

--- leaderboard.php
+++ leaderboard.php
@@ -10,6 +10,8 @@
 <br/>
 <?
   $timestamp = (isset($_GET['t']) && is_numeric($_GET['t']) ? (int)$_GET['t'] : 0);
+  $rid = (isset($_GET['rid']) && is_numeric($_GET['rid'])) ? (int)$_GET['rid'] : -1;
+  $tid = (isset($_GET['tid']) && is_numeric($_GET['tid'])) ? (int)$_GET['tid'] : -1;
   $req = (isset($_GET['req']) ? $_GET['req'] : 'leaderboard');
   $possibleDir = TOURNAMENTS_DIR . $timestamp;
   if (is_dir($possibleDir)) {
@@ -17,23 +19,23 @@
       $possibleFile = TOURNAMENTS_DIR . $timestamp . '/' . LEADERBOARD_FILE;
       maybeEchoFile($possibleFile, false);
     }
-    else if ($req === 'stdout') {
-      $possibleFile = TOURNAMENTS_DIR . $timestamp . '/' . STDOUT_FILE;
+    else if ($req === 'stdout' && $rid > 0 && $tid > 0) {
+      $possibleFile = TOURNAMENTS_DIR . $timestamp . '/round' . $rid . '/table' . $tid . '/' .STDOUT_FILE;
       maybeEchoFile($possibleFile, true);
     }
     else if ($req === 'all_messages') {
       echo "<p> all_messages.log.gz download not supported yet. </p>";
     }
-    else if ($req === 'player0') {
-      $possibleFile = TOURNAMENTS_DIR . $timestamp . '/player0.log';
+    else if ($req === 'player0' && $rid > 0 && $tid > 0) {
+      $possibleFile = TOURNAMENTS_DIR . $timestamp . '/round' . $rid . '/table' . $tid . '/player0.log';
       maybeEchoFile($possibleFile, true);
     }
-    else if ($req === 'player1') {
-      $possibleFile = TOURNAMENTS_DIR . $timestamp . '/player1.log';
+    else if ($req === 'player1' && $rid > 0 && $tid > 0) {
+      $possibleFile = TOURNAMENTS_DIR . $timestamp . '/round' . $rid . '/table' . $tid . '/player1.log';
       maybeEchoFile($possibleFile, true);
     }
-    else if ($req === 'player2') {
-      $possibleFile = TOURNAMENTS_DIR . $timestamp . '/player2.log';
+    else if ($req === 'player2' && $rid > 0 && $tid > 0) {
+      $possibleFile = TOURNAMENTS_DIR . $timestamp . '/round' . $rid . '/table' . $tid . '/player2.log';
       maybeEchoFile($possibleFile, true);
     }
   }
